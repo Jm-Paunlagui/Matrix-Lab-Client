c5634825dec61aa46d70057c2b24ea3b993430ae
Jm-Paunlagui <paunlagui.jm.cs@gmail.com>
2022-08-27 02:07:44 +0800

    Revision: Added bg-no-repeat

--- client/src/views/auth/Auth.js
+++ client/src/views/auth/Auth.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import visionary from "../../assets/img/visionary_technology.svg";
 import bg from '../../assets/img/register_bg_2.png';
 const Auth = () => {
     return (
         <>
             <div className="relative w-full h-full min-h-screen py-40 bg-blue-100 font-Montserrat">
-                <div className="absolute top-0 w-full h-full blur-lg">
+                <div className="absolute top-0 w-full h-full bg-no-repeat bg-full blur-lg">
                     <img src={bg} alt="bg" className="object-contain w-full h-full" />
                 </div>
                 <Outlet />
