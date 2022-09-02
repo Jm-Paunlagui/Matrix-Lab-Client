6bcfb63d22a19facd7df8fc3699df573ad447364
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-09-03 01:09:21 +0800

    Navbar proper format?

--- client/src/components/navbars/navbar.jsx
+++ client/src/components/navbars/navbar.jsx
@@ -24,49 +24,32 @@ const Navbar = () => {
             <nav className="fixed top-0 flex flex-wrap items-center justify-between w-full p-1 shadow-md backdrop-blur-xl bg-white/50 font-Montserrat">
                 <div className="container flex flex-wrap items-center justify-between mx-auto max-w-7xl">
                     <div className="flex justify-between w-full lg:w-auto lg:static lg:block lg:justify-start">
-                        <NavLink to="/" 
-                            className="flex items-center px-3 py-2 text-gray-900 rounded-md hover:text-blue-900">
+                        <NavLink to="/" className="flex items-center px-3 py-2 text-gray-900 rounded-md hover:text-blue-900">
                             <img src={logo} alt="logo" className="w-10 h-10 mr-2" />
                             <h1 className="hidden ml-3 font-bold tracking-widest text-md md:flex">MATRIX LAB</h1>
                             <h1 className="ml-3 font-bold tracking-widest text-md md:hidden">MATRIX LAB</h1>
                         </NavLink>
-                        <button
-                            className="block px-3 py-1 text-xl leading-none bg-transparent border border-transparent border-solid rounded outline-none cursor-pointer lg:hidden focus:outline-none"
-                            type="button"
-                            onClick={() => setNavbarOpen(!navbarOpen)}
-                        >
+                        <button type="button" onClick={() => setNavbarOpen(!navbarOpen)} className="block px-3 py-1 text-xl leading-none bg-transparent border border-transparent border-solid rounded outline-none cursor-pointer lg:hidden focus:outline-none" >
                             <GoThreeBars color="#374151" />
-                            
                         </button>
                     </div>
-                        <div
-                            className={
-                                "lg:flex flex-grow items-center  lg:bg-opacity-0 lg:shadow-none" +
-                                (navbarOpen ? " block" : " hidden")
-                            }
-                            id="example-navbar-warning"
-                        >
+                        <div className={ "lg:flex flex-grow items-center  lg:bg-opacity-0 lg:shadow-none" + (navbarOpen ? " block" : " hidden")}>
                         <ul className="flex flex-col justify-start list-none lg:flex-row lg:ml-auto">
-                            <NavLink to ="/"
-                                    className="text-gray-700">
+                            <NavLink to ="/" className="text-gray-700">
                                 <li className="flex items-center px-8 py-4 hover:text-blue-900">
                                     <AiFillHome size={24} title="Home"/>
                                     <h1 className="block px-8 text-sm font-medium tracking-wider blockml-3 lg:hidden">Home</h1>
                                 </li>
                             </NavLink>
-                            <NavLink to ="leaderboard"
-                                    
-                                    className="text-gray-700">
+                            <NavLink to ="leaderboard" className="text-gray-700">
                                 <li className="flex items-center px-8 py-4 hover:text-blue-900">
                                     <MdLeaderboard size={24} title="Leaderboard"/>
                                     <div className="block px-8 text-sm font-medium tracking-wider blockml-3 lg:hidden">Leaderboard</div>
                                 </li>
                             </NavLink>
-                            <NavLink to ={link}
-                                    className="text-gray-700">
+                            <NavLink to ={link} className="text-gray-700">
                                 <li className="flex items-center px-8 py-4 hover:text-blue-900">
                                     {navIcon}
-                                    
                                     <div className="block px-8 text-sm font-medium tracking-wider blockml-3 lg:hidden">{tokenAuth ? "Dashboard" : "Sign-in"}</div>
                                 </li>
                             </NavLink>
