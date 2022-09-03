36fba67f3afa57774683fa2fd68eb2a700c225dc
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-09-03 18:34:43 +0800

    Navbar component

--- client/src/views/Landing.js
+++ client/src/views/Landing.js
@@ -1,12 +1,38 @@
 import Footer from "../components/footers/footer";
-import Navbar from "../components/navbars/navbar";
+import Navbar from "../components/navbars/propsNavbar";
 import { Outlet } from "react-router-dom";
 import React from "react";
+import logo from "../assets/img/android-chrome-192x192.png";
 
 const Landing = () => {
+  
+  const items = [
+    {
+      id: 1,
+      titlel: "Home",
+      link: "/",     
+    },
+    {
+      id: 2,
+      titlel: "Dashboard",
+      link: "admin",
+    },
+    {
+      id: 3,
+      titlel: "Sign-in",
+      link: "auth",
+    },
+    {
+      id: 4,  
+      titlel: "Leaderboard",
+      link: "leaderboard",
+    },
+  ]
+
   return (
     <>
-      <Navbar />
+      <Navbar main={"/"} logo={logo} 
+      title={"MATRIX LAB"} navitems={items} />
       <div className="bg-white">
         <Outlet />
       </div>
