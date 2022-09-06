991c8d3ab48fa81fd494137c5560c9419a42a612
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-09-06 22:17:17 +0800

    Missing an explicit type attribute for button

--- client/src/components/buttons/button.component.jsx
+++ client/src/components/buttons/button.component.jsx
@@ -27,7 +27,7 @@ function Button({
       <i className={icon_style} /> {title}
     </button>
   ) : (
-    <button className={style}>
+    <button className={style} type="button">
       <Link
         style={{ width: "100%", height: "100%", padding: "16px" }}
         to={href}
