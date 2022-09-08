0cd440c74caae9da553be0a50a8b4dedf94cf552
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-09-08 23:17:57 +0800

    Added Ranking

---
+++ client/src/views/pub/Ranking.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Ranking() {
+  return (
+      <div className="flex flex-col items-center justify-center h-screen pt-14 font-Montserrat">
+        <div className="container items-center mx-auto max-w-7xl">
+          <h1 className="text-4xl font-bold text-center">Ranking</h1>
+        </div>
+      </div>
+  )
+}
