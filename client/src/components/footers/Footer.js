3637047a33de54b8e616b596628ad3b2e485acba
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-09-11 00:10:05 +0800

    Footer update

--- client/src/components/footers/Footer.js
+++ client/src/components/footers/Footer.js
@@ -70,8 +70,8 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        <div className="flex flex-wrap text-center">
-          <div className="items-center w-full md:flex md:space-x-52 place-content-center">
+        <div className="flex flex-wrap text-center py-4">
+          <div className="items-center w-full md:flex md:space-x-20 lg:place-content-end lg:px-4 xl:px-44 place-content-center">
             {UsefulLinks.map((link) => (
               <Link to={`/${link.link}`} key={link.name}>
                 <div className="block pb-2 text-sm font-semibold transition duration-300 ease-linear delay-150 hover:text-gray-800">
