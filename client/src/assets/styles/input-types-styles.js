b1f913c73f34dfef6683c14c9fa729de5145f27d
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-09-13 00:21:17 +0800

    update, transition

--- client/src/assets/styles/input-types-styles.js
+++ client/src/assets/styles/input-types-styles.js
@@ -1,10 +1,10 @@
 export const TEXT_FIELD = `w-full p-4 text-sm font-medium tracking-wider placeholder-gray-500 bg-gray-100 border border-gray-200 rounded-lg focus:outline-none focus:border-gray-400 focus:bg-white`;
 
-export const DELAY_1 = "transition-colors duration-300 ease-in-out delay-150";
-export const DELAY_3 = "transition-colors duration-300 ease-in-out delay-300";
-export const DELAY_5 = "transition-colors duration-300 ease-in-out delay-500";
-export const DELAY_7 = "transition-colors duration-300 ease-in-out delay-700";
-export const DELAY_9 = "transition-colors duration-300 ease-in-out delay-1000";
+export const DELAY_1 = "transition duration-300 ease-in-out delay-150";
+export const DELAY_3 = "transition duration-300 ease-in-out delay-300";
+export const DELAY_5 = "transition duration-300 ease-in-out delay-500";
+export const DELAY_7 = "transition duration-300 ease-in-out delay-700";
+export const DELAY_9 = "transition duration-300 ease-in-out delay-1000";
 export const ICON_PLACE_SELF_CENTER = "pr-2 place-self-center";
 export const ICON_PLACE_SELF_CENTER_1 = "place-self-center";
 
