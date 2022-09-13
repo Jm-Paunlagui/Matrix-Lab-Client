88d8a8261d9538cb46ea1b6351be9100271cf541
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-09-13 15:33:10 +0800

    activestyle

--- client/src/components/navbars/AdminNavigationBar.js
+++ client/src/components/navbars/AdminNavigationBar.js
@@ -1,4 +1,4 @@
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { NavLink, useResolvedPath } from "react-router-dom";
 import React, { Fragment } from "react";
@@ -54,21 +54,21 @@ export default function AdminNavigationBar(to) {
 
   /**
    * @description Handles the profile menu for the admin pages
-   * @type {[{name: string, href: string},{name: string, href: string}]}
+   * @type {[{name: string, href: string},{name: string, href: string}, {name: string, href: string}]}
    */
   const user_controllers = [
-    { name: "Your Profile", href: "/admin/profile" },
-    { name: "Sign out", href: "admin/logout" },
+    { name: "Your Profile", href: "/admin/profile", current: isActive("/admin/profile") },
+    { name: "Sign out", href: "admin/logout", current: isActive("/admin/logout") },
   ];
   return (
-    <Disclosure as="nav" className="bg-gray-800 shadow-md font-Montserrat">
+    <Disclosure as="nav" className="shadow-md backdrop-blur-xl bg-white/50 font-Montserrat">
       {({ open }) => (
         <>
           <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-500 rounded-md hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block w-6 h-6" aria-hidden="true" />
@@ -99,7 +99,7 @@ export default function AdminNavigationBar(to) {
                           className={`${
                             item.current
                               ? "bg-gray-900 text-white"
-                              : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                              : "text-gray-900 hover:bg-gray-700 hover:text-white"
                           } block px-3 py-2 rounded-md text-base font-medium`}
                         >
                           {item.name}
@@ -111,18 +111,10 @@ export default function AdminNavigationBar(to) {
               </div>
 
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="p-1 text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="w-6 h-6" aria-hidden="true" />
-                </button>
-
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="flex text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Menu.Button className={`flex text-sm rounded-full  ${isActive("/admin/profile") ? "ring-2 ring-gray-900 ring-offset-4 ring-offset-blue-100" : ""}`}>
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="w-8 h-8 rounded-full"
@@ -190,7 +182,7 @@ export default function AdminNavigationBar(to) {
                       className={`${
                         item.current
                           ? "bg-gray-900 text-white"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                          : "text-gray-900 hover:bg-gray-700 hover:text-white"
                       } block px-3 py-2 rounded-md text-base font-medium`}
                     >
                       {item.name}
