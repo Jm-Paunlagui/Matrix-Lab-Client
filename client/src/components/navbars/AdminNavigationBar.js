f71e6c8b2e2c53018604ccbce98976d0414a92b2
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-09-20 23:11:15 +0800

    AdminNavigationBar modified

--- client/src/components/navbars/AdminNavigationBar.js
+++ client/src/components/navbars/AdminNavigationBar.js
@@ -1,9 +1,10 @@
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
 import { NavLink, useResolvedPath } from "react-router-dom";
 import React, { Fragment } from "react";
 
 import logo from "../../assets/img/android-chrome-192x192.png";
+import { DELAY_1 } from "../../assets/styles/input-types-styles";
 
 export default function AdminNavigationBar(to) {
   /**
@@ -57,36 +58,25 @@ export default function AdminNavigationBar(to) {
    * @type {[{name: string, href: string},{name: string, href: string}, {name: string, href: string}]}
    */
   const user_controllers = [
-    {
-      name: "Your Profile",
-      href: "/admin/profile",
-      current: isActive("/admin/profile"),
-    },
-    {
-      name: "Sign out",
-      href: "admin/logout",
-      current: isActive("/admin/logout"),
-    },
+    { name: "Your Profile", href: "/admin/profile", current: isActive("/admin/profile") },
+    { name: "Sign out", href: "admin/logout", current: isActive("/admin/logout") },
   ];
   return (
-    <Disclosure
-      as="nav"
-      className="shadow-md backdrop-blur-xl bg-white/50 font-Montserrat"
-    >
+    <Menu as="nav" className="fixed top-0 w-full shadow-md backdrop-blur-xl bg-white/50 font-Montserrat">
       {({ open }) => (
         <>
           <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-500 rounded-md hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Menu.Button className="inline-flex items-center justify-center p-2 text-gray-500 rounded-md hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block w-6 h-6" aria-hidden="true" />
                   ) : (
                     <Bars3Icon className="block w-6 h-6" aria-hidden="true" />
                   )}
-                </Disclosure.Button>
+                </Menu.Button>
               </div>
               <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
                 <div className="flex items-center flex-shrink-0">
@@ -111,7 +101,7 @@ export default function AdminNavigationBar(to) {
                             item.current
                               ? "bg-gray-900 text-white"
                               : "text-gray-900 hover:bg-gray-700 hover:text-white"
-                          } block px-3 py-2 rounded-md text-base font-medium`}
+                          } block px-3 py-2 rounded-md text-base font-medium ${DELAY_1}`}
                         >
                           {item.name}
                         </h5>
@@ -125,13 +115,7 @@ export default function AdminNavigationBar(to) {
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button
-                      className={`flex text-sm rounded-full ring-2 hover:ring-indigo-900 ring-offset-4 ${
-                        isActive("/admin/profile")
-                          ? "ring-gray-900 ring-offset-white"
-                          : ""
-                      }`}
-                    >
+                    <Menu.Button className={`flex text-sm rounded-full ring-2 hover:ring-indigo-900 ring-offset-4 ${isActive("/admin/profile") ? "ring-gray-900 ring-offset-white" : ""}`}>
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="w-8 h-8 rounded-full"
@@ -191,7 +175,6 @@ export default function AdminNavigationBar(to) {
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95"
           >
-            <Disclosure.Panel className="sm:hidden">
               <div className="px-2 pt-2 pb-3 space-y-1">
                 {navigation.map((item) => (
                   <NavLink key={item.name} to={item.href}>
@@ -200,17 +183,16 @@ export default function AdminNavigationBar(to) {
                         item.current
                           ? "bg-gray-900 text-white"
                           : "text-gray-900 hover:bg-gray-700 hover:text-white"
-                      } block px-3 py-2 rounded-md text-base font-medium`}
+                      } block px-3 py-2 rounded-md text-base font-medium ${DELAY_1}`}
                     >
                       {item.name}
                     </h5>
                   </NavLink>
                 ))}
               </div>
-            </Disclosure.Panel>
           </Transition>
         </>
       )}
-    </Disclosure>
+    </Menu>
   );
 }
