7a504edee055a9144e5b9d5fbbe41c18c8986e7f
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-09-21 14:48:24 +0800

    User Profile sample

--- client/src/views/admin/IndexAdmin.js
+++ client/src/views/admin/IndexAdmin.js
@@ -1,6 +1,7 @@
 import AdminNavigationBar from "../../components/navbars/AdminNavigationBar";
 import { Outlet } from "react-router-dom";
 import React from "react";
+import bg from "../../assets/img/register_bg_2.png";
 
 /**
  * @description Handles admin pages for the application
@@ -8,10 +9,10 @@ import React from "react";
 export default function IndexAdmin() {
   return (
     <>
-      <AdminNavigationBar />
-      <div>
-        <Outlet />
-      </div>
+        <AdminNavigationBar />
+        <div className="mt-16 bg-white font-Montserrat">
+            <Outlet />
+        </div>
     </>
   );
 }
