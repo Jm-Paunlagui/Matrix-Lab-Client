198fffd228df52f3b186b9ce17552a00fcea9f94
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-09-24 20:00:29 +0800

    Update to navabar

--- client/src/components/navbars/AdminNavigationBar.js
+++ client/src/components/navbars/AdminNavigationBar.js
@@ -121,7 +121,7 @@ export default function AdminNavigationBar(to) {
                 </div>
               </div>
 
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 static inset-auto ml-6 pr-0">
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
