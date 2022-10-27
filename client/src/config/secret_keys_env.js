ec3592c006ed2db50ce3af2388873434e3d26450
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-10-27 23:34:40 +0800

    deleted

--- client/src/config/secret_keys_env.js
+++
@@ -1,2 +0,0 @@
-export const MATRIX_RSA_PUBLIC_KEY = `-----BEGIN PUBLIC KEY-----\n${process.env.REACT_APP_MATRIX_RSA_PUBLIC_KEY}\n-----END PUBLIC KEY-----`;
-export const MATRIX_ALGORITHM = process.env.REACT_APP_MATRIX_ALGORITHM;
