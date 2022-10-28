262938d4115a31ebb0f0963a343eaf0472217cf2
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-10-29 04:08:55 +0800

    RFC5322-compliant Regular Expression that covers 99.99% of input email addresses.

--- client/src/helpers/Helper.js
+++ client/src/helpers/Helper.js
@@ -26,3 +26,12 @@ export const maskUsername = (username) => {
 
   return `${username.slice(0, 2)}**${username.slice(-1)}`;
 };
+
+/**
+ * @description Regex to validate the email address with RFC 5322 Official Standard.
+ * @type {RegExp}
+ */
+export const emailRegex = new RegExp(
+    // eslint-disable-next-line no-useless-escape,no-control-regex
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])/g
+);
