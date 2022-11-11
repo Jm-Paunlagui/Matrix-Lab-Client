07781a130f86bf8433e5c61805d265b724e0632f
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-11-11 21:03:16 +0800

    Loading page

---
+++ client/src/components/loading/LoadingPage.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import {LOADING_ANIMATION} from "../../assets/styles/input-types-styles";
+
+/**
+ * @description Logout in the loading animation
+ */
+export default function LoadingPage() {
+    return (
+        <div className="flex items-center mx-auto w-full h-96">
+            <div className="h-full mx-auto font-Montserrat">
+                <div className="w-full flex items-center content-center justify-center h-full">
+                    <div className="flex flex-col justify-center animate-pulse">
+                        <div className={`px-5 py-1 pl-4 flex flex-row justify-center`}>
+                            {LOADING_ANIMATION()}
+                            {/* Animated text */}
+                            <h6 className="text-xl font-bold text-black">
+                                Getting data
+                            </h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
